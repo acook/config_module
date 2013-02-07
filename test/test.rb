ad0073eaf8b53c2f90f366dee3bc52914b120cb7
Anthony Cook <anthonymichaelcook@gmail.com>
2013-02-07 11:29:17 -0800

    Cleaned up test file so it makes more sense.

--- test/test.rb
+++ test/test.rb
@@ -1,12 +1,11 @@
-module Rails; def self.env; 'production'; end; end
-
-Dir.chdir File.dirname(__FILE__)
+require_relative 'mutest'
+extend Mutest
 
 require_relative '../config_module'
 require_relative 'example_config'
 
-require_relative 'mutest'
-extend Mutest
+module Rails; def self.env; 'production'; end; end
+Dir.chdir File.dirname(__FILE__)
 
 spec 'modules extended with ConfigModule will load configurations' do
   ExampleConfig.foo == 'bar'
