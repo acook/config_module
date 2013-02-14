d4b26c708ed5a773e0d6389399b02516fed6c643
Anthony Cook <anthonymichaelcook@gmail.com>
2013-02-14 15:44:15 -0800

    Put the module in the correct place in lib.

--- lib/config_module.rb
+++ lib/config_module.rb
@@ -1,5 +1,43 @@
-require "config_module/version"
+require 'config_module/version'
+require 'ostruct'
+require 'yaml'
 
 module ConfigModule
-  # Your code goes here...
+  def [] key
+    config.send key
+  end
+
+  def config
+    @config ||= load_config
+  end
+
+  def config_file file
+    @config_file = file
+  end
+
+  def namespace name
+    @namespace = name
+  end
+
+protected
+
+  def wrap data
+    if data.is_a? Hash then
+      ConfigOption.new data
+    else
+      data
+    end
+  end
+
+  def load_config
+    file = YAML.load_file(@config_file)
+    wrap @namespace ? (file[@namespace] || file[@namespace.to_sym]) : file
+  end
+
+  def method_missing name, *args, &block
+    wrap(config.send name, *args, &block) || super
+  end
+
+  class ConfigOption < OpenStruct
+  end
 end
