015443761071263257ffacea8dd8663d57d804df
Anthony Cook <anthonymichaelcook@gmail.com>
2013-03-13 18:24:43 -0700

    Moved ConfigOption specs into the correct file.

--- spec/config_helper_spec.rb
+++ spec/config_helper_spec.rb
@@ -1,16 +1,6 @@
 require_relative 'spec_helper'
 
-hash = {a: {b: 5}}
-opt = ConfigModule::ConfigOption.new hash
+helper = ConfigModule::ConfigHelper.new
+helper.config_file = './config/example.yml'
 
-spec 'ConfigOptions are Enumerable' do
-  opt.map{|k,v| v[:b]} == [5]
-end
 
-spec 'to_ary' do
-  begin
-    opt.to_ary
-  rescue NoMethodError => error
-    error.class == ConfigModule::ConfigOption::NotFoundError
-  end
-end
--- spec/config_option_spec.rb
+++ spec/config_option_spec.rb
@@ -1,10 +1,25 @@
 require_relative 'spec_helper'
 
+hash = {a: {b: 5}}
+opt = ConfigModule::ConfigOption.new hash
+
 spec 'responds to #[]' do
-  ConfigModule::ConfigOption.new.respond_to? :[]
+  opt.respond_to? :[]
 end
 
 spec '#[] returns value associated with key' do
-  opt = ConfigModule::ConfigOption.new key: 'value'
-  opt[:key] == 'value'
+  opt[:a] == hash[:a]
 end
+
+spec 'ConfigOptions are Enumerable' do
+  opt.map{|k,v| v[:b]} == [5]
+end
+
+spec 'to_ary' do
+  begin
+    opt.to_ary
+  rescue NoMethodError => error
+    error.class == ConfigModule::ConfigOption::NotFoundError
+  end
+end
+
