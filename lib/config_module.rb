361b388102cee3ee6801dd9fc649f29d97c6d9da
Anthony Cook <github@anthonymcook.com>
2015-10-03 21:34:00 -0500

    Setup method takes options in a block DSL.

--- lib/config_module.rb
+++ lib/config_module.rb
@@ -46,4 +46,50 @@ private
   def respond_to_missing? name, include_all
     __config_module_helper.respond_to_missing_handler(name, include_all) || super
   end
+
+  module_function
+
+  def setup &block
+    options = {
+      method_name: 'config',
+      path: './config/settings.yml',
+    }
+
+    setup_dsl = Class.new do
+      def method_name new_name
+        options[:method_name] = new_name
+      end
+
+      def path new_path
+        options[:path] = new_path
+      end
+
+      def namespaces new_namespaces
+        options[:namespaces] = new_namespaces.flatten
+      end
+
+      def options
+        @options ||= Hash.new
+      end
+    end
+
+    if block_given? then
+      options.merge! setup_dsl.new.tap{|dsl| dsl.instance_eval &block}.options
+    end
+
+    Module.new do
+      @options = options
+
+      define_method options[:method_name] do
+        __config_module_helper.config
+      end
+
+      def self.extended child
+        child.extend ConfigModule
+        helper = child.send(:__config_module_helper)
+        helper.config_file = @options[:path]
+        helper.namespaces  = @options[:namespaces]
+      end
+    end
+  end
 end
